e79cbad333c9108b549ff05d2dc9eb44566847a4
Awwab <mawwabkhank2006@gmail.com>
2024-03-03 13:48:37 +0500

    encoding generated

--- gen_encoding.py
+++ gen_encoding.py
@@ -13,4 +13,16 @@ for path in img_names:
     std_images.append(cv2.imread(os.path.join(folder_path, path)))
     std_ids.append(os.path.splitext(path)[0])
 
-print(std_ids)
+def generate_encoding(std_images):
+    encodings = []
+    for img in std_images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encoding = face_recognition.face_encodings(img)[0]
+        encodings.append(encoding)
+
+    return encodings
+
+print("Encoding Started ...")
+encoding_list = generate_encoding(std_images)
+print(encoding_list)
+print("Encoding Complete")
