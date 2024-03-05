301aa403547d61a1388947b115e64a685e0ff8ef
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-05 18:20:28 +0500

    student images stored in firebase bucket

--- gen_encoding.py
+++ gen_encoding.py
@@ -2,6 +2,9 @@ import cv2
 import pickle
 import face_recognition
 import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import  storage
 
 #importing students images
 folder_path = 'Images'
@@ -9,9 +12,22 @@ img_names = os.listdir(folder_path)
 std_images = []
 std_ids = []
 
+cred = credentials.Certificate("firebase_key.json")
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://facial-attendance-realtime-default-rtdb.firebaseio.com/",
+    "storageBucket": "facial-attendance-realtime.appspot.com"
+
+})
+
+
 for path in img_names:
     std_images.append(cv2.imread(os.path.join(folder_path, path)))
     std_ids.append(os.path.splitext(path)[0])
+
+    fileName = f'{folder_path}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
 
 def generate_encoding(std_images):
     encodings = []
