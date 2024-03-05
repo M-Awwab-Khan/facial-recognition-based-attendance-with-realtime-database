16714f783a5be60b875469c553f211ece8e8f9f0
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-05 18:20:10 +0500

    more data stored

--- add_data_to_database.py
+++ add_data_to_database.py
@@ -18,7 +18,37 @@ data = {
         "standing": "E",
         "year": 1,
         "last_attendance_time": "2024-03-05 18:05:53"
-    }
+    },
+    "321654":
+        {
+            "name": "Murtaza Hassan",
+            "major": "Robotics",
+            "batch": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "852741":
+        {
+            "name": "Emly Blunt",
+            "major": "Economics",
+            "batch": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "batch": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
 }
 
 for k, v in data.items():
